e2c45e3f10acfdf573b3d1408f65abb931b934d1
Harshvardhan123156 <harshvardhansinghrathore614@gmail.com>
2026-03-27 19:02:59 +0530

    work on conf and enviroment variables

---
+++ Blogweb/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+  
+  console.log(import.meta.env.Vite_APPWRITE_URL);
+
+  return (
+    <>
+    <h1>Welcome to Blogweb</h1>
+    </>
+  )
+}
+
+export default App
